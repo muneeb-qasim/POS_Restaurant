63983f380c20077b3d433d94438d94d0ddbc95f2
muneeb <muneebbutt5962@gmail.com>
2020-12-17 17:33:15 +0500

    For Data

--- src/index.js
+++ src/index.js
@@ -42,7 +42,7 @@ ReactDOM.render(
 
       <Route path="/admin" component={Admin} />
       <Route path="/rtl" component={RTL} />
-      <Redirect from="/" to="/admin/dashboard" />
+      <Redirect from="/" to="/Login" />
     </Switch>
   </Router>,
   document.getElementById("root")
